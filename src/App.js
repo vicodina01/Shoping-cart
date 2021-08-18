de6be1a7b1a77cedbab7a1f1ee4bd30da78d6a2e
vicodina01 <mybi@live.com.mx>
2021-08-18 18:56:11 -0500

    Readme & comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
-import Cart from "./components/Cart";
-import Catalog from "./components/Catalog";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import Cart from './components/Cart';
+import Catalog from './components/Catalog';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
-  const [order, setOrder] = useState("asc");
+  const [order, setOrder] = useState('asc');
   const [showStock, setShowStock] = useState(true);
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState('');
   const [showCart, setShowCart] = useState(false);
 
   useEffect(() => {
     let url =
-      "https://my-json-server.typicode.com/alerodriguez162/JSON-Example/db";
+      'https://my-json-server.typicode.com/alerodriguez162/JSON-Example/db';
 
+    //fecth data from API and add some atributtes
     const fetchData = async () => {
       try {
         const response = await fetch(url);
@@ -31,15 +32,18 @@ function App() {
 
         setData(products);
       } catch (error) {
-        console.log("error", error);
+        console.log('error', error);
       }
     };
     fetchData();
   }, []);
 
+  //Sort column data function
   const sortBy = (col) => {
-    if (order === "asc") {
+    if (order === 'asc') {
+      //sort ascendant
       const sortedData = [...data].sort((a, b) => {
+        //check if the value is not a number
         let aCol = isNaN(a[col]) ? a[col].toLowerCase() : a[col];
         let bCol = isNaN(b[col]) ? b[col].toLowerCase() : b[col];
 
@@ -47,9 +51,11 @@ function App() {
       });
 
       setData(sortedData);
-      setOrder("desc");
+      setOrder('desc');
     } else {
+      //sort descendant
       const sortedData = [...data].sort((a, b) => {
+        //check if the value is not a number
         let aCol = isNaN(a[col]) ? a[col].toLowerCase() : a[col];
         let bCol = isNaN(b[col]) ? b[col].toLowerCase() : b[col];
 
@@ -57,10 +63,11 @@ function App() {
       });
 
       setData(sortedData);
-      setOrder("asc");
+      setOrder('asc');
     }
   };
 
+  //select item and update quantity
   const selectItem = (status, id) => {
     let quantity = status ? 1 : 0;
     setData(
@@ -70,16 +77,19 @@ function App() {
     );
   };
 
+  //update quantity input
   const updateSelectItem = (quantity, id) => {
     const newData = [...data];
     const index = data.findIndex((products) => products.name === id);
 
+    //if aquantity change , set status selected
     newData[index].status = quantity > 0 ? true : false;
 
+    //validate quantity over stock
     if (newData[index].stock >= quantity) {
       newData[index].quantity = quantity;
     } else {
-      alert("The quantity of products exceeds the stock");
+      alert('The quantity of products exceeds the stock');
       newData[index].quantity = newData[index].stock;
     }
 
@@ -90,12 +100,12 @@ function App() {
     <Container>
       <Row>
         <Col>
-          <Card className="card-main">
+          <Card className='card-main'>
             <Card.Body>
               <Card.Title>
                 <h3>CATÁLOGO</h3>
               </Card.Title>
-              <Card.Subtitle className="text-muted">
+              <Card.Subtitle className='text-muted'>
                 Listado de Productos
               </Card.Subtitle>
               <hr />
